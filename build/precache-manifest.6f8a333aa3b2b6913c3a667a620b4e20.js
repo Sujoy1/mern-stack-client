d72bb2af2e5407515e9d7719386b5337448e6fa5
sujoyseal11 <sujoyseal11@gmail.com>
2021-05-01 00:13:35 +0530

    Done

---
+++ build/precache-manifest.6f8a333aa3b2b6913c3a667a620b4e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3536df376c69685092ead580ee4f9ef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f55c5e796f609db3991",
+    "url": "/static/css/main.6160d39a.chunk.css"
+  },
+  {
+    "revision": "221a82726e591e203c1e",
+    "url": "/static/js/2.1ebae321.chunk.js"
+  },
+  {
+    "revision": "b52534a5ffbcfa5f4ac7a4883223c24a",
+    "url": "/static/js/2.1ebae321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f55c5e796f609db3991",
+    "url": "/static/js/main.0bd2bb81.chunk.js"
+  },
+  {
+    "revision": "69aff06ab0ffa4f5fe72",
+    "url": "/static/js/runtime-main.e9ef9ac7.js"
+  }
+]);
